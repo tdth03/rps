52c3c2a253d7e35ec45f481ef8a4aeb0a79f3fb7
t-rav <tbharmon03@gmail.com>
2021-04-15 08:51:34 -0500

    Fixed round to 5 points function.

--- scripts.js
+++ scripts.js
@@ -20,7 +20,16 @@ const SELECTIONS = [
         beats: 'paper'
     }
 ]
+const modal = document.querySelector('.game-results');
+const modalWinner = document.querySelector('.game-winner');
+const closeBtn = document.getElementById('close');
+const winnerText = document.querySelector('.win-text');
 
+let yourScore = '0';
+let computerSCore = '0';
+
+
+/* Click Event Listener For User Selection */
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
         const selectionName = selectionButton.dataset.selection;
@@ -31,6 +40,18 @@ selectionButtons.forEach(selectionButton => {
     
 })
 
+/*Random Selection for Computer Selection */
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
+}
+
+/* Take Computer Selection and User Selection, compare, and return winner */
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name;
+}
+
+/* Take winning selections, add results, and increment score if needed. */
 function makeSelection(selection) {
     const computerSelection = randomSelection();
     const yourWinner = isWinner(selection, computerSelection);
@@ -42,20 +63,25 @@ function makeSelection(selection) {
     if (yourWinner) incrementScore(yourScoreSpan);
     if (computerWinner) incrementScore(computerScoreSpan);
 
+    yourScore = parseInt(yourScoreSpan.innerText);
+    computerScore = parseInt(computerScoreSpan.innerText);
+
+    console.log("Your score is " + yourScoreSpan.innerText);
+    console.log("Computer score is " + computerScoreSpan.innerText);
+
     console.log(computerSelection.name);
 
     console.log("You selected " + selection.name +". Computer selected " + computerSelection.name + ".")
 
-    
+    if (yourScore === 5 || computerScore === 5) declareWinner();
 }
 
+/* Increment score function */
 function incrementScore(scoreSpan) {
-    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
-    console.log("Your score is " + yourScoreSpan.innerText);
-    console.log("Computer score is " + computerScoreSpan.innerText);
-    if (yourScoreSpan.innerText === 5 || computerScoreSpan.innerText === 5) declareWinner();
+    scoreSpan.innerText = parseInt(scoreSpan.innerText, 10) + 1
 }
 
+/* Add results to page, adding classes to include CSS styling. */
 function addSelectionResult(selection, winner) {
     const div = document.createElement('div')
     div.innerText = selection.emoji
@@ -64,37 +90,31 @@ function addSelectionResult(selection, winner) {
     finalColumn.after(div)
 }
 
-function isWinner(selection, opponentSelection) {
-    return selection.beats === opponentSelection.name;
-}
-
-function randomSelection() {
-    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
-    return SELECTIONS[randomIndex]
-}
-
-
+/* Declare winner after user or computer reaches 5 round wins. */
 function declareWinner() {
-    /* const modal = document.querySelector('.game-results');
-    const modalWinner = document.querySelector('.game-winner');
-    const closeBtn = document.getElementById('close');
-    const winnerText = document.querySelector('.win-text'); */
-
-    if (playerScoreSpan.innerText > computerScoreSpan.innerText) {
-        /* modal.style.display = fixed; */
-        alert("Congratulations! You have won.");
+    yourScoreSpan.innerText = parseInt(yourScoreSpan.innerText);
+    computerScoreSpan.innerText = parseInt(computerScoreSpan.innerText);
+    if (yourScoreSpan.innerText > computerScoreSpan.innerText) {
+        modal.style.display = 'block';
+        winnerText.innerText = "Congratulations! You have won " + yourScore + " to " + computerScore;
     } else {
-        /* modal.style.display = fixed; */
-        alert("You suck. You have failed us all.");
+        modal.style.display = 'block';
+        winnerText.innerText = "You suck. You have failed us all. The Computer beat you " + computerScore + " to " + yourScore;
     }
 
-    /* closeBtn.onclick = function() {
-        modal.style.display = none;
+    selectionButtons.forEach((selection) => {
+        selection.classList.add('end-game');
+        selection.classList.remove('selection:hover');
+        selection.setAttribute('disabled', '');
+    });
+
+    closeBtn.onclick = function() {
+        modal.style.display = "none";
       }
       
     window.onclick = function(event) {
         if (event.target == modal) {
           modal.style.display = "none";
         }
-    } */
+    }
 }
